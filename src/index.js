5c780b2b5c6be5a7337fff003f34323ea1337f44
Alison Williams <apw5011@gmail.com>
2021-08-23 15:05:42 -0400

    initial page setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("testing...");
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM is Loaded");
+
+    loadButton();
+})
+
+function loadButton() {
+    // debugger
+    const mainDiv = document.getElementById('main');
+    const button = document.createElement('button');
+    button.innerText = "Click me!";
+    mainDiv.append(button);
+    button.addEventListener('click', initiateCounter)
+}
+
+function initiateCounter() {
+    const mainDiv = document.getElementById('main');
+    const counter = document.createElement('p');
+    counter.innerText = "0";
+    mainDiv.append(counter);
+}
