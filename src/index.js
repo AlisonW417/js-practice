732fb7989a805913261d588484cfde012b1b5236
Alison Williams <apw5011@gmail.com>
2021-08-23 15:24:16 -0400

    started counter function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM is Loaded");
 
+    const plusButton = document.getElementById('plus');
+    plusButton.addEventListener('click', plusClicked);
+
+    const minusButton = document.getElementById('minus');
+    minusButton.addEventListener('click', minusClicked);
+
     // loadButton();
 })
+
+function plusClicked() {
+    console.log("I clicked plus!")
+
+}
+
+function minusClicked() {
+    console.log("I clicked minus!")
+
+}
 
 
 
