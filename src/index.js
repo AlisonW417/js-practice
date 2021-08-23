8bcaca3f3a98bc578d86df2f9bcf35a6a0fc8904
Alison Williams <apw5011@gmail.com>
2021-08-23 15:19:44 -0400

    updated html

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM is Loaded");
 
-    loadButton();
+    // loadButton();
 })
 
-function loadButton() {
-    // debugger
-    const mainDiv = document.getElementById('main');
-    const button = document.createElement('button');
-    button.innerText = "Click me!";
-    mainDiv.append(button);
-    button.addEventListener('click', initiateCounter)
-}
 
-function initiateCounter() {
-    const mainDiv = document.getElementById('main');
-    const counter = document.createElement('p');
-    counter.innerText = "0";
-    mainDiv.append(counter);
-}
+
+// function loadButton() {
+//     // debugger
+//     const mainDiv = document.getElementById('main');
+//     const button = document.createElement('button');
+//     button.innerText = "Click me!";
+//     mainDiv.append(button);
+//     button.addEventListener('click', initiateCounter)
+// }
+
+// function initiateCounter() {
+//     const mainDiv = document.getElementById('main');
+//     const counter = document.createElement('p');
+//     counter.innerText = "0";
+//     mainDiv.append(counter);
+// }
