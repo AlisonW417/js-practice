e30f37042e91dedf599398165fa1cf8a9022964d
Alison Williams <apw5011@gmail.com>
2022-09-18 12:18:51 -0400

    big O section

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM is Loaded");
     console.log(addUpTo(6));
+    console.log(addUpTo2(6));
+    
+    /*
+    var t1 = performance.now();
+    addUpTo(100000000);
+    var t2 = performance.now();
+    console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
 
-
+    var t3 = performance.now();
+    addUpTo2(100000000);
+    var t4 = performance.now();
+    console.log(`Time Elapsed: ${(t4 - t3) / 1000} seconds.`);
+    */
 })
 
 function addUpTo(n) {
@@ -13,4 +24,8 @@ function addUpTo(n) {
         total += i;
     }
     return total;
+}
+
+function addUpTo2(n) {
+    return n * (n + 1) / 2;
 }
